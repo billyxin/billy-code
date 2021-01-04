2f60a28ee9a713c0e5b0a056c32e0d2f929b8119
billyxin <5113724@qq.com>
2021-01-05 07:49:56 +0800

    1

--- elestudy/win/renderer.js
+++ elestudy/win/renderer.js
@@ -1,6 +1,37 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
+window.$ = window.jquery = window.jQuery = require('jquery');
+const { remote, ipcRenderer } = require('electron');
+const fs = require('fs');
+const path = require('path');
+$(() => {
+	console.log('--------------from index.html--------');
+
+	ipcRenderer.on('mode', (event, msg) => {
+		$('#home3').text(msg);
+	});
+
+	// json package name
+	let data = fs.readFileSync(path.join(__dirname, './../package.json'));
+	data = JSON.parse(data.toString())['name'];
+
+	//path
+	let cwd = process.cwd();
+	let real_path = process.env.PORTABLE_EXECUTABLE_DIR;
+
+	$('#jsonname').val(data);
+
+	$('#real').text(real_path);
+	$('#realarea').val(real_path);
+	$('#cwd').text(cwd);
+	$('#home2').text(process.env.NODE_ENV);
+
+	if (process.env.NODE_ENV === 'dev') {
+		$('#home').text('dev');
+	} else if (process.env.NODE_ENV === 'product') {
+		$('#home').text('product');
+	} else {
+		alert('not any env');
+		alert(process.env.NODE_ENV);
+	}
+
+	$('h1').text('这里h1')
+});
