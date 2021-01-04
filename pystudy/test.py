99d429efc5a86bdf2e785166f94e6cbd78fc3b5e
billyxin <5113724@qq.com>
2021-01-04 17:55:06 +0800

    11

--- pystudy/test.py
+++ pystudy/test.py
@@ -1,67 +1,28 @@
+# try:
+# 	print('try...')
+# 	i = 20/0
+# 	print(i)
+# # except ZeroDivisionError as e:
+# # 	print('error',e)
+# except ValueError as v:
+# 	print('error:',v)
+# else:
+# 	print('everthing good...')
+# finally:
+# 	print('finally...')
 #%%
-def log(fn):
-        # def wrapper(*argv, **kws):
-        print('start go log.')
-        #     return fn(*argv, **kws)
-        print('log end.')
-        return fn
+import logging
 
+def pr():
+	i=20/0
+	print(i)
+	print('thxxxxx')
 
-class animal(object):
-    def __init__(self, name):
-        self.name = name
+def main():
+	try:
+		pr()
+	except Exception as e:
+		logging.exception(e)
+main()
 
-    # def log(fn):
-    #     def wrapper(*argv, **kws):
-    #         print('start go log.')
-    #         return fn(*argv, **kws)
-    #         print('log end.')
-    #     return wrapper
-
-    @log
-    def aname(self, subname):
-        print('print name is:', self.name + '---' + subname)
-        return self.name
-
-    def bite(self, words):
-        print(words)
-
-    def __str__(self):
-        return 'the name is: ' + self.name
-
-
-dog = animal('wangwang')
-print(dog)
-dog.bite('gogogo')
-n = dog.aname('mysub...........')
-print(n)
-print('*'*28)
-print(dir(animal))
-
-dir(animal)
-# ['__class__',
-#  '__delattr__',
-#  '__dict__',
-#  '__dir__',
-#  '__doc__',
-#  '__eq__',
-#  '__format__',
-#  '__ge__',
-#  '__getattribute__',
-#  '__gt__',
-#  '__hash__',
-#  '__init__',
-#  '__init_subclass__',
-#  '__le__',
-#  '__lt__',
-#  '__module__',
-#  '__ne__',
-#  '__new__',
-#  '__reduce__',
-#  '__reduce_ex__',
-#  '__repr__',
-#  '__setattr__',
-#  '__sizeof__',
-#  '__str__',
-#  '__subclasshook__',
-#  '__weakref__',
+print('gogogogo')
